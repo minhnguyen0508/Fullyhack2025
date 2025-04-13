002dafbb26442a9d9995d8b72265cbaa9a2782f9
Minh Nguyen <minhtnnguyen0508@gmail.com>
2025-04-12 23:57:05 -0700

    Fixing Electron Forge related error

--- space-hacks/webpack.rules.js
+++ space-hacks/webpack.rules.js
@@ -21,20 +21,4 @@ module.exports = [
     test: /\.(png|jpe?g|gif|svg)$/i,
     type: 'asset/resource',
   },
-  // Put your webpack loader rules in this array.  This is where you would put
-  // your ts-loader configuration for instance:
-  /**
-   * Typescript Example:
-   *
-   * {
-   *   test: /\.tsx?$/,
-   *   exclude: /(node_modules|.webpack)/,
-   *   loaders: [{
-   *     loader: 'ts-loader',
-   *     options: {
-   *       transpileOnly: true
-   *     }
-   *   }]
-   * }
-   */
-];
+ ];
