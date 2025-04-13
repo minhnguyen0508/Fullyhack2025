26fab7c7012d3cd9e4ec8c22c6540786c413c94d
zaffinabrianna <zaffinabrianna@yahoo.com>
2025-04-13 11:25:52 -0700

    fixed img

--- space-hacks/src/renderer.js
+++ space-hacks/src/renderer.js
@@ -1,5 +1,5 @@
 import './index.css';
-import planetGif from './assets/saturnFullyHacks.png';
+import planetGif from './assets/planetFullyHacks.gif';
 
 const img = document.createElement('img');
 img.src = planetGif;
