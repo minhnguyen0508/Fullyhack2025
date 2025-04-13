f1b5db008c637656060ab322ae77eac27cf6abc2
zaffinabrianna <zaffinabrianna@yahoo.com>
2025-04-13 11:39:30 -0700

    added change day func to change days dependding n month

--- space-hacks/src/renderer.js
+++ space-hacks/src/renderer.js
@@ -16,3 +16,42 @@ if (container) {
 
 document.getElementById('centeranimation').appendChild(img);
 console.log('👋 This message is being logged by "renderer.js", included via webpack'); 
+
+// Func to update days
+window.updatedays = function()
+{
+    const daySelect = document.getElementById("day");
+    const monthSelect = document.getElementById("month");
+    const month = parseInt(monthSelect.value);
+
+    daySlect.innerHTML = '<option value = ""> SELECT DAY</option>';
+
+    const daysInMonth = 
+    {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31
+    };
+
+    if (month)
+    {
+        const days = daysInMonth[month];
+        for (let i = 1; i <= days; i++)
+        {   
+            const option = documnet.createElement("option");
+            option.value = i;
+            option.text = i;
+            daySelect.appendChild(option);
+        }
+    }
+
+}
